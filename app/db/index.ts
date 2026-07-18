55bddcc2a02e8e8c2dc23ae4d45cd7a65abc09d9
ale2306-dev <ale.2306.dev@gmail.com>
2026-07-17 23:19:00 -0400

    Primer commit

--- app/db/index.ts
+++ app/db/index.ts
@@ -1,6 +1,6 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
 process.loadEnvFile()
-const uri = process.env.MONGO_DB_URI;
+const uri = process.env.MONGO_DB_URI as string;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
